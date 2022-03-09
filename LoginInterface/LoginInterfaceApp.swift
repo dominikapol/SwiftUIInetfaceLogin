ddcc7a9a0d547b4c7d85cc1324fc46b4710b2c36
Dominika <dominikapiganova@mail.ru>
2022-03-09 11:51:33 +0200

    Initial Commit

---
+++ LoginInterface/LoginInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginInterfaceApp.swift
+//  LoginInterface
+//
+//  Created by Dominika Poleshyck on 9.03.22.
+//
+
+import SwiftUI
+
+@main
+struct LoginInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
